6863cdad8c3e108cb37d8f936354dc2a44554c5a
derryev <derryev@ssfs.org>
2024-01-12 14:15:30 -0500

    Working on unit nine final card project V2

--- deck.py
+++ deck.py
@@ -2,8 +2,15 @@
 class Deck:
     def __init__(self):
         self.card_deck = []
-        self.rank = []
+        self.rank = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q","K","A"]
         self.suit = ["♣"️️,"♦","❤"️,"♠"]
+        for card_rank in self.rank:
+            self.card_deck.append(card_rank)
+            for suit in self.suit:
+                self.card_deck
+
+
+
 
     def shuffle_deck():
 
